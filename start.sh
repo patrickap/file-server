bd69717d7e2a0d9b9314d410aa0654a5a23eaa04
patrickap <patrick.schlageter@web.de>
2022-12-17 19:46:22 +0100

    chore: added named volumes

--- start.sh
+++ start.sh
@@ -5,11 +5,6 @@ set -a
 source .env
 set +a
 
-# create folders and permissions for bind mounts
-sudo mkdir -p data/sftpgo data/radicale
-sudo chown -R 1000:1000 data/sftpgo
-sudo chown -R 2999:2999 data/radicale
-
 # create dns zone for cloud.local
 envsubst '${HOST_IP}' < db.cloud.local.template > db.cloud.local
 
