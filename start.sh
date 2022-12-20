5e6a72db194f5f76b1baab9670c5aa622b3976fc
patrickap <patrick.schlageter@web.de>
2022-12-20 21:42:39 +0100

    fix: coredns could not load zone file

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ source .env
 set +a
 
 # create dns zone from template
-envsubst '${HOST_IP} ${HOST_NAME}' < .zone.template > .zone
+envsubst '${HOST_IP} ${HOST_NAME}' < db.domain.tld.template > db.domain.tld
 
 # start containers
 docker compose up -d
