f1b6e4f48a31ea66f916ba2ae99eee3ca27f6eaa
patrickap <patrick.schlageter@web.de>
2022-12-23 21:33:25 +0100

    fix: trash action whitespace bug

--- sftpgo/trash_action.sh
+++ sftpgo/trash_action.sh
@@ -4,16 +4,17 @@ username=$SFTPGO_ACTION_USERNAME
 filepath=$SFTPGO_ACTION_PATH
 basename=$(basename -- "$filepath")
 
-# if the filepath includes username/.trash the action
-# was triggered inside the trash folder
+# naive check: if the filepath includes username/.trash the action 
+# was triggered inside the trash folder which is allowed
+# this will also cause the deletion of files and folders named .trash*
 if [[ "$filepath" == *"$username/.trash"* ]]; then
   # allow deletion and execute delete action
   exit 1
 else
   # create trash folder if missing
-  mkdir -p /srv/sftpgo/data/$username/.trash
+  mkdir -p "/srv/sftpgo/data/$username/.trash"
   # move and increment identical filenames instead of overwriting them
-  mv --backup=numbered $filepath /srv/sftpgo/data/$username/.trash/$basename
+  mv --backup=t "$filepath" "/srv/sftpgo/data/$username/.trash/$basename"
   # deny deletion and skip delete action
   exit 0  
 fi
