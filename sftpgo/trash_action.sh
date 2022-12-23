31d7a200943a9dd24c3e5c5ed93cf1758eedd92d
patrickap <patrick.schlageter@web.de>
2022-12-23 20:33:02 +0100

    feat: improved sftpgo trash action

--- sftpgo/trash_action.sh
+++ sftpgo/trash_action.sh
@@ -2,14 +2,18 @@
 
 username=$SFTPGO_ACTION_USERNAME
 filepath=$SFTPGO_ACTION_PATH
-filename=$(basename -- "$filepath")
+basename=$(basename -- "$filepath")
 
-# TODO: handle file vs folder deletion
-# TODO: fix files inside trash can't be deleted
-
-mkdir -p /srv/sftpgo/data/${username}/.trash
-
-# move and increments identical filenames instead of overwriting them
-mv --backup=t ${filepath} /srv/sftpgo/data/${username}/.trash/${filename}
-
-exit 0
+# if the filepath includes username/.trash the action
+# was triggered inside the trash folder
+if [[ "$filepath" == *"$username/.trash"* ]]; then
+  # allow deletion and execute delete action
+  exit 1
+else
+  # create trash folder if missing
+  mkdir -p /srv/sftpgo/data/$username/.trash
+  # move and increment identical filenames instead of overwriting them
+  mv --backup=numbered $filepath /srv/sftpgo/data/$username/.trash/$basename
+  # deny deletion and skip delete action
+  exit 0  
+fi
